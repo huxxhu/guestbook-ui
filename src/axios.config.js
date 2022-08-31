333a83cc35fd6298154d054a56d1ed97cbd09117
h <huhuqaq@gmail.com>
2022-08-31 19:35:45 +0800

    🔧 chore: update api url

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://guestbook-api-coral.vercel.app',
+  baseURL: 'https://guestbook-api-coral.vercel.app/api',
 })
 
 export default instance
