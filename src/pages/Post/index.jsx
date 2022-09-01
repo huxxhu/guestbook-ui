39112e8763a946560ce092ea2d57e3cafad5c19f
h <huhuqaq@gmail.com>
2022-09-01 16:39:22 +0800

    ✨  feat: add reply

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -5,7 +5,7 @@ import { FaPencilAlt } from 'react-icons/fa'
 
 import { getPosts } from '../../api/posts'
 
-const PostCard = ({ name, content, createdAt, ip, ...props }) => {
+const PostCard = ({ name, content, createdAt, ip, reply, ...props }) => {
   const dateFormat = new Date(createdAt).toLocaleString('zh-TW')
   return (
     <Box borderRadius='2rem' p={5} color='gray.900' bg='white' boxShadow='lg.green'>
@@ -18,10 +18,26 @@ const PostCard = ({ name, content, createdAt, ip, ...props }) => {
         {name}
       </Text>
       <Text whiteSpace='pre-line'>{content}</Text>
-      <Divider my='2' borderColor='green.400' borderStyle='solid' />
-      <Text fontSize='xs' color='gray.700' align='right'>
+      <Text
+        fontSize='xs'
+        color='gray.600'
+        align='right'
+        display='flex'
+        alignItems='center'
+        whiteSpace='nowrap'
+        mt={2}
+        _before={{ content: `""`, height: '1px', bg: 'green.400', width: '100%', display: 'block', mr: '1em' }}
+      >
         {dateFormat} - 來自: {ip}
       </Text>
+      {reply && (
+        <Box borderRadius='1em' mt={2} p={3} bg='green.50' color='green.400' fontSize='sm'>
+          <Text as='span' mb='1' fontWeight='600'>
+            版主回覆：
+          </Text>
+          <Text whiteSpace='pre-line'>{reply.content}</Text>
+        </Box>
+      )}
     </Box>
   )
 }
@@ -56,7 +72,14 @@ const Post = () => {
       <VStack spacing={6} align='stretch' opacity={isLoading ? '0.4' : '1'} transition='opacity 0.3s'>
         {postData.data?.length > 0 &&
           postData.data?.map((data) => (
-            <PostCard key={data._id} name={data.name} content={data.content} createdAt={data.createdAt} ip={data.ip} />
+            <PostCard
+              key={data._id}
+              name={data.name}
+              content={data.content}
+              createdAt={data.createdAt}
+              ip={data.ip}
+              reply={data.reply}
+            />
           ))}
       </VStack>
       {postData.pagination?.pageTotal > 1 && (
